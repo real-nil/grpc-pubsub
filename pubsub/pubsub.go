9442810ed1f19c8dd73fd91c649c2251c5f4238e
dmitryrpm <admin@suwer.ru>
2017-06-01 10:31:05 +0300

    add first pubsub

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -1,3 +1,73 @@
 package pubsub
 
-type server struct{}
+import (
+	ppubsub "github.com/real-nil/grpc-pubsub/proto/pubsub"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/grpclog"
+	"sync"
+)
+
+type Sender interface {
+	Send(*ppubsub.Data) error
+}
+
+type server struct {
+	lockForPubsuber *sync.RWMutex
+	pubsubClinet    map[string][]Sender
+}
+
+func New() *server {
+	return &server{
+		lockForPubsuber: &sync.RWMutex{},
+		pubsubClinet:    make(map[string][]Sender, 0),
+	}
+}
+
+/*
+        Subscribe(*Topic, PubSub_SubscribeServer) error
+	Publish(context.Context, *Data) (*Answer, error)
+*/
+
+func (s *server) Subscribe(top *ppubsub.Topic, stream ppubsub.PubSub_SubscribeServer) error {
+	grpclog.Printf("start subscribe for topic %s", top.Name)
+	//c := stream.Context()
+	s.lockForPubsuber.Lock()
+	s.pubsubClinet[top.Name] = append(s.pubsubClinet[top.Name], stream)
+	s.lockForPubsuber.Unlock()
+	// check context, return error if Done
+	//stream, err := client.New(context.Background())
+	//if err := nil{
+	//	grpclog.Printf("incorrect create stream %s", stream)
+	//	return errors.New("ololo")
+	//}
+
+	return nil
+}
+
+func (s *server) Publish(ctx *context.Context, data *ppubsub.Data) (*ppubsub.Answer, error) {
+	var (
+		senders []Sender
+		ok      bool
+	)
+	s.lockForPubsuber.RLock()
+	senders, ok = s.pubsubClinet[data.Topic]
+	s.lockForPubsuber.RUnlock()
+
+	if !ok {
+		// OR send error return nil, errors.New()
+		return &ppubsub.Answer{Subsriptions: 0}, nil
+	}
+
+	var c int32
+	c = int32(len(senders))
+
+	for _, stream := range senders {
+		err := stream.Send(data)
+		if err != nil {
+			grpclog.Printf("incorrect streamt")
+			c--
+		}
+	}
+
+	return &ppubsub.Answer{Subsriptions: c}, nil
+}
